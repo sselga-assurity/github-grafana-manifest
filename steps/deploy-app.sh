62465eb7e8a070c496602f75a0e52ef38752d1ed
Seph Selga <158115435+sselga-assurity@users.noreply.github.com>
2024-04-10 15:13:08 +0800

    Update deploy-app.sh

--- steps/deploy-app.sh
+++ steps/deploy-app.sh
@@ -3,5 +3,5 @@
 # script copied from ird-cf-app-deployments with few mods
 
 export CF_DIAL_TIMEOUT=30
-cf login -a ${{ secrets.CF_API }} -u ${{secrets.CF_USERNAME}} -p ${{secrets.CF_PASSWORD}} -o ${{secrets.CF_ORG}} -s ${{secrets.CF_SPACE}}
+cf login -a ${{secrets.CF_API}} -u ${{secrets.CF_USERNAME}} -p ${{secrets.CF_PASSWORD}} -o ${{secrets.CF_ORG}} -s ${{secrets.CF_SPACE}}
 cf push
