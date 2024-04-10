48255ddb8b0e52062f4d719fdd825394f3a3409f
Seph Selga <158115435+sselga-assurity@users.noreply.github.com>
2024-04-10 15:43:40 +0800

    Update deploy-app.sh

--- steps/deploy-app.sh
+++ steps/deploy-app.sh
@@ -2,12 +2,6 @@
 
 # script copied from ird-cf-app-deployments with few mods
 
-CF_API="${{ secrets.CF_API }}"
-CF_USERNAME="${{ secrets.CF_USERNAME }}"
-CF_PASSWORD="${{ secrets.CF_PASSWORD }}"
-CF_ORG="${{ secrets.CF_ORG }}"
-CF_SPACE="${{ secrets.CF_SPACE }}"
-
 export CF_DIAL_TIMEOUT=30
-cf login -a ${CF_API} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o ${CF_ORG} -s ${CF_SPACE}
+cf login -a https://api.sys.ap.assurity.cloud/ -u IA8rzOBz-le+ENLtN8Icd6,3pr34CnIb -p IA8rzOBz-le+ENLtN8Icd6,3pr34CnIb -o msd -s ctaas
 cf push
