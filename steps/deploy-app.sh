cff281bd0a8bd4ba06373f717f7cc5b5bfa9c586
Seph Selga <158115435+sselga-assurity@users.noreply.github.com>
2024-04-10 14:10:31 +0800

    Update deploy-app.sh

--- steps/deploy-app.sh
+++ steps/deploy-app.sh
@@ -3,5 +3,5 @@
 # script copied from ird-cf-app-deployments with few mods
 
 export CF_DIAL_TIMEOUT=30
-${{secrets.GITHUB_WORKSPACE}}/cf login -a ${{ secrets.CF_API }} -u ${{secrets.CF_USERNAME}} -p ${{secrets.CF_PASSWORD}} -o ${{secrets.CF_ORG}} -s ${{secrets.CF_SPACE}}
-${{secrets.GITHUB_WORKSPACE}}/cf push -f ${{secrets.GITHUB_WORKSPACE}}/manifests/${{secrets.APP_NAME}}.yml
+${{secrets.CF_WORKSPACE}}/cf login -a ${{ secrets.CF_API }} -u ${{secrets.CF_USERNAME}} -p ${{secrets.CF_PASSWORD}} -o ${{secrets.CF_ORG}} -s ${{secrets.CF_SPACE}}
+${{secrets.CF_WORKSPACE}}/cf push -f ${{secrets.GITHUB_WORKSPACE}}/manifests/${{secrets.APP_NAME}}.yml
