fffbee5dd071fbad6574ece02db615cd45a4438b
aaronabarentos <158115343+aaronabarentos@users.noreply.github.com>
2024-04-11 10:48:38 +0800

    Update deploy-app.sh

--- steps/deploy-app.sh
+++ steps/deploy-app.sh
@@ -2,6 +2,15 @@
 
 # script copied from ird-cf-app-deployments with few mods
 
+set -e
+
+: ${CF_API:?required}
+: ${CF_USERNAME:?required}
+: ${CF_PASSWORD:?required}
+: ${CF_ORG:?required}
+: ${CF_SPACE:?required}
+: ${GITHUB_WORKSPACE:?required}
+
 export CF_DIAL_TIMEOUT=30
 ${GITHUB_WORKSPACE}/cf login -a ${CF_API} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o ${CF_ORG} -s ${CF_SPACE}
 ${GITHUB_WORKSPACE}/cf push
