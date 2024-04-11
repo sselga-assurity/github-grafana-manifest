a20c90136e6b86b649e6abfabf26bc416d90c449
aaronabarentos <158115343+aaronabarentos@users.noreply.github.com>
2024-04-11 10:41:12 +0800

    Update deploy-app.sh

--- steps/deploy-app.sh
+++ steps/deploy-app.sh
@@ -3,5 +3,5 @@
 # script copied from ird-cf-app-deployments with few mods
 
 export CF_DIAL_TIMEOUT=30
-${GITHUB_WORKSPACE}/cf login -a https://api.sys.ap.assurity.cloud/ -u 0f92c60f-d158-47b2-99db-c25bce7faf43 -p IA8rzOBz-le+ENLtN8Icd6,3pr34CnIb -o msd -s ctaas
+${GITHUB_WORKSPACE}/cf login -a ${CF_API} -u ${CF_USERNAME} -p ${CF_PASSWORD} -o ${CF_ORG} -s ${CF_SPACE}
 ${GITHUB_WORKSPACE}/cf push
